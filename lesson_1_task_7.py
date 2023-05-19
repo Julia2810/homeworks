0de4f933c274911c01f0ddbc748a72065d4fefeb
Julia2810 <juliajudina2810@gmail.com>
2023-05-19 17:56:29 +0300

    Добавили 3-ий файл

---
+++ lesson_1_task_7.py
@@ -0,0 +1,43 @@
+def telefon_number(char):
+    print(char[0], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[1], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[2], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[3], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[4], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[5], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[6], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[7], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[8], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[9], end ='')
+telefon_number('88005553535')
+
+def telefon_number(char):
+    print(char[10], end ='')
+telefon_number('88005553535')
---
+++ lesson_1_task_8.py
@@ -0,0 +1,13 @@
+def new_function(num):
+    print(num, end = '')
+new_function(8)
+new_function(8)
+new_function(0)
+new_function(0)
+new_function(5)
+new_function(5)
+new_function(5)
+new_function(3)
+new_function(5)
+new_function(3)
+new_function(5)
