203820463b70caefc9ec7226220d7f6b3f199790
Julia2810 <juliajudina2810@gmail.com>
2023-05-26 16:17:54 +0300

    Домашняя работа 2

---
+++ lesson_2_task_1.py
@@ -0,0 +1,3 @@
+lst = ['grape', 'peach', 'pear', 'orange', 'banana', 'apple']
+print(f'First: {lst[0]}')
+print(f'Last: {lst[len(lst)-1]}')
---
+++ lesson_2_task_3.py
@@ -0,0 +1,4 @@
+def square(side):
+    square = int(side*side)
+    print(f'Площадь квадрата равна: {square}')
+square(5.2)
