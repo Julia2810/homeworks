6932c0bbf50f002a40a172fe76157df4ac7c131d
Julia2810 <juliajudina2810@gmail.com>
2023-05-28 16:00:47 +0300

    Домашняя работа 2

--- lesson_2_task_5.py
+++ lesson_2_task_5.py
@@ -1,24 +1,12 @@
 def month_to_season(month):
-    1 == 'January'
-    2 == 'February'
-    3 == 'March'
-    4 == 'April'
-    5 == 'May'
-    6 == 'June'
-    7 == 'July'
-    8 == 'August'
-    9 == 'September'
-    10 == 'October'
-    11 == 'November'
-    12 == 'December'
-    
-    if month == 1 or month == 2 or month == 12:
+
+    if month in [1, 2, 12]:
         print("Зима")
-    elif month == 3 or month == 4 or month == 5:
+    elif month in [3, 4, 5]:
         print("Весна")
-    elif month == 6 or month == 7 or month == 8:
+    elif month in [6, 7, 8]:
         print("Лето")
     else:
         print("Осень")
-month_to_season(10)
+month_to_season(3)
     
