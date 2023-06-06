c2aaa0ceeb8e96da4d83b71ad81b142e9e50c49f
Julia2810 <juliajudina2810@gmail.com>
2023-06-06 16:15:56 +0300

    ДЗ3

--- ДЗ3/lesson_3_task_3.py
+++ ДЗ3/lesson_3_task_3.py
@@ -1,11 +1,12 @@
 from address import Address
 from mailing import Mailing
 
-class Mailing:
-    to_address = ('440001', 'Москва', 'Ленина', 3, 45)
-    from_address = ('880001', 'Ижевск', 'Калинина', 6, 84)
-    track = 1234567
-    cost = 598
-    print(f'Отправление {track} из {index}, {city}, {street}, {house} - {apartment} в {index}, {city}, {street}, {house} -{apartment}. Стоимость {cost} рублей.')
+to_address = Address(440001, 'Москва', 'Ленина', 3, 56)
+from_address = Address(880001, 'Ижевск', 'Калинина', 6, 45)
+
+my_mailing = Mailing(to_address, from_address, 123565, '123564')
+
+
+print(f'Отправление: {my_mailing.track} из {from_address.index}, {from_address.city}, {from_address.street}, {from_address.house} - {from_address.apartment} в {to_address.index}, {to_address.city}, {to_address.street}, {to_address.house} - {to_address.apartment}. Стоимость {my_mailing.cost} рублей.')
     
     
