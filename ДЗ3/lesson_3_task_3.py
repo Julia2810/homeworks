04cafc6b6a2f37f8055afc698cb287805e97e474
Julia2810 <juliajudina2810@gmail.com>
2023-06-07 09:44:59 +0300

    ДЗ3

--- ДЗ3/lesson_3_task_3.py
+++ ДЗ3/lesson_3_task_3.py
@@ -7,6 +7,6 @@ from_address = Address(880001, 'Ижевск', 'Калинина', 6, 45)
 my_mailing = Mailing(to_address, from_address, 123565, '123564')
 
 
-print(f'Отправление: {my_mailing.track} из {from_address.index}, {from_address.city}, {from_address.street}, {from_address.house} - {from_address.apartment} в {to_address.index}, {to_address.city}, {to_address.street}, {to_address.house} - {to_address.apartment}. Стоимость {my_mailing.cost} рублей.')
+print(f'Отправление: {my_mailing.track} из {my_mailing.from_address.index}, {my_mailing.from_address.city}, {my_mailing.from_address.street}, {my_mailing.from_address.house} - {my_mailing.from_address.apartment} в {my_mailing.to_address.index}, {my_mailing.to_address.city}, {my_mailing.to_address.street}, {my_mailing.to_address.house} - {my_mailing.to_address.apartment}. Стоимость {my_mailing.cost} рублей.')
     
     
--- ДЗ3/mailing.py
+++ ДЗ3/mailing.py
@@ -1,12 +1,11 @@
 class Mailing:
-    
-    class Address:
-    
-        def __init__(self, to_address, from_address, cost: int, track: str):
-            self.to_address = self.Address
-            self.from_address = self.Address
-            self.cost = cost
-            self.track = track
+
+
+    def __init__(self, to_address, from_address, cost: int, track: str):
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
             
         
     
