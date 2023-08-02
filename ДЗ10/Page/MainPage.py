3dfa3fb945f8494675e6c7cc6aa150806107bd5f
Julia2810 <juliajudina2810@gmail.com>
2023-08-02 13:10:40 +0300

    ДЗ10

---
+++ ДЗ10/Page/MainPage.py
@@ -0,0 +1,70 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import allure
+
+
+class MainPage:
+    def __init__(self, driver):
+        self._new_list = []
+        self._driver = driver
+        self._driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+    @allure.step("Заполнение поля First name: {word}")
+    def input_first_name(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="first-name"]').send_keys(word)
+
+    @allure.step("Заполнение поля Last name: {word}")
+    def input_last_name(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="last-name"]').send_keys(word)
+
+    @allure.step("Заполнение поля Address: {word}")
+    def input_address(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="address"]').send_keys(word)
+
+    @allure.step("Заполнение поля E-mail: {word}")
+    def input_e_mail(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="e-mail"]').send_keys(word)
+
+    @allure.step("Заполнение поля Phone number: {word}")
+    def input_phone(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="phone"]').send_keys(word)
+
+    @allure.step("Заполнение поля City: {word}")
+    def input_city(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="city"]').send_keys(word)
+
+    @allure.step("Заполнение поля Country: {word}")
+    def input_country(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="country"]').send_keys(word)
+
+    @allure.step("Заполнение поля Job position: {word}")
+    def input_job_position(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="job-position"]').send_keys(word)
+
+    @allure.step("Заполнение поля Company: {word}")
+    def input_company(self, word: str):
+        self._driver.find_element(By.CSS_SELECTOR, 'input[name="company"]').send_keys(word)
+
+    @allure.step("Нажатие на кнопку Submit")
+    def click_submit(self):
+        self._driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+    @allure.step("Получение цвета поля Zip-code")
+    def test_red_color(self) -> str:
+        color = self._driver.find_element(By.ID, "zip-code").value_of_css_property('background-color')
+        return color
+
+    @allure.step("Получение цвета полей First name, Last name, Address, E-mail, Phone number, City, Country, Job position, Company")
+    def test_green_color(self) -> str:
+        new_list = [self._driver.find_element(By.ID, "first-name").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "last-name").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "address").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "e-mail").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "phone").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "city").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "country").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "job-position").value_of_css_property('background-color'),
+                    self._driver.find_element(By.ID, "company").value_of_css_property('background-color')]
+
+        for element in new_list:
+            return element
---
+++ ДЗ10/Page/test_colors_of_fields.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from MainPage import MainPage
+import allure
+
+@allure.title("Заполнение полей формы")
+@allure.description("Проверка, что поля подсвечиваются нужными нам цветами")
+@allure.feature("CREATE")
+@allure.severity("HIGH")
+def test_input():
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    main_page = MainPage(browser)
+
+    with allure.step("Вводим имя в поле First name"):
+        main_page.input_first_name('Иван')
+
+    with allure.step("Вводим фамилию в поле Last name"):
+        main_page.input_last_name('Петров')
+
+    with allure.step("Вводим адрес в поле Adress"):
+        main_page.input_address('Ленина, 55-3')
+
+    with allure.step("Вводим адрес эл. почты в поле E-mail"):
+        main_page.input_e_mail('test@skypro.com')
+
+    with allure.step("Вводим номер телефона в поле Phone number"):
+        main_page.input_phone('+7985899998787')
+
+    with allure.step("Вводим название города в поле City"):
+        main_page.input_city('Москва')
+
+    with allure.step("Вводим название страны в поле Country"):
+        main_page.input_country('Россия')
+
+    with allure.step("Вводим название должности в поле Job position"):
+        main_page.input_job_position('QA')
+
+    with allure.step("Вводим название компании в поле Company"):
+        main_page.input_company('SkyPro')
+
+    with allure.step("Нажимаем на кнопку Submit"):
+        main_page.click_submit()
+
+    with allure.step("Проверка, что поле Zip-code подсвечивается красным цветом"):
+        assert main_page.test_red_color() == 'rgba(248, 215, 218, 1)'
+
+    with allure.step("Проверка, что поля First name, Last name, Address, E-mail, Phone number, City, Country, Job position, Company подсвечиваются зеленым цветом"):
+        assert main_page.test_green_color() == 'rgba(209, 231, 221, 1)'
