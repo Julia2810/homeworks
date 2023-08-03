472eb7598c24bf599d1ae1a6cb6cd7d9c6a2e158
Julia2810 <juliajudina2810@gmail.com>
2023-08-03 13:00:13 +0300

    ДЗ10

--- ДЗ10/Calculator/MainPageCalculator.py
+++ ДЗ10/Calculator/MainPageCalculator.py
@@ -15,7 +15,7 @@ class Calculator:
             self._driver.find_element(By.ID, "delay").clear()
 
     @allure.step("Введение количества секунд")
-    def input_secund(self, number):
+    def input_secund(self, number: str):
         self._driver.find_element(By.ID, "delay").send_keys(number)
 
     @allure.step("Нажатие на кнопку с цифрой 7")
@@ -35,10 +35,10 @@ class Calculator:
         self._driver.find_element(By.XPATH,"//span[contains(text(),'=')]").click()
 
     @allure.step("Ожидание драйвера в течение {secunds} секунд до появления результата {result_of_action}")
-    def wait_result(self, secunds, result_of_action):
+    def wait_result(self, secunds: int, result_of_action: str):
         self._WebDriverWait(self._driver, secunds).until(EC.text_to_be_present_in_element((By.CLASS_NAME, 'screen'), result_of_action))
 
     @allure.step("Получение результата")
-    def test_result_of_sum(self):
+    def test_result_of_sum(self)-> str:
         res = self._driver.find_element(By.CLASS_NAME, "screen").text
         return res
